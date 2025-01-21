9c2a7ffc5feb33f92b9092501a975209ea5b8d05
gallopeladoprofe <juanftp100.edu@gmail.com>
2025-01-21 19:24:24 -0300

    uso de variables y constantes

---
+++ js/variables.js
@@ -0,0 +1,19 @@
+// variables
+// MALA PRÁCTICA
+var persona1 = {
+    nombres: "Juan"
+    , apellidos: "Vera"
+    , anhos: 164
+};
+
+// buena práctica
+let nombres = "Francisco Solano El Pollo Lopez";
+let costo_presupuesto = 63;
+
+// constantes
+const nocambia = false;
+
+console.log(typeof(persona1), persona1);
+console.log(typeof(nombres), nombres);
+console.log(typeof(nocambia), nocambia);
+console.log(typeof(costo_presupuesto), costo_presupuesto);

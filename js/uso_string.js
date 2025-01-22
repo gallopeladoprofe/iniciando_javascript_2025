b67f4644793f8ef3cd0d9495c06b419601098a7e
gallopeladoprofe <juanftp100.edu@gmail.com>
2025-01-22 19:28:08 -0300

    uso de strings

---
+++ js/uso_string.js
@@ -0,0 +1,14 @@
+const comillaSimple = 'comilla Simple';
+const comillaDoble = "comilla doble";
+
+// template string
+const tmp = `Voy a construir un parrafo
+ usando las otras dos constantes definidas
+ arriba. Estas son ${comillaSimple} y ${comillaDoble}
+`;
+
+const chamburreado = "A veces me siento muy fotosintensis 'hee'";
+const chamburreado2 = 'Epa, "puchero"';
+
+console.log(tmp);
+console.log(chamburreado);

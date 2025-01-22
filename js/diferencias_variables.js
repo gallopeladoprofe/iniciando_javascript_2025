728d06154c98cc5bce82d524069bb621d9e56e15
gallopeladoprofe <juanftp100.edu@gmail.com>
2025-01-22 18:45:54 -0300

    agregaciones y pruebas del debugger

--- js/diferencias_variables.js
+++ js/diferencias_variables.js
@@ -3,30 +3,40 @@
 
 // var
 function ejemploVar() {
+    debugger;
     if(true) {
+        debugger;
         var numero = 10;
     }
+    debugger;
     console.log(numero);
 }
 
 function ejemploLet() {
+    debugger;
     if(true) {
+        debugger;
         let numero = 10;
+        debugger;
     }
+    debugger;
     console.log(numero);
 }
 
 function ejemploConst() {
+    debugger;
     const z = 30;
+    debugger;
     //z = 15;
     console.log(z);
 
     const array = [1, 2, 3];
     array.push(4);
+    array.push("Asunción");
     console.log(array);
-    //array = [];
+    array = [];
 }
 
-ejemploVar();
+//ejemploVar();
 //ejemploLet();
 ejemploConst();

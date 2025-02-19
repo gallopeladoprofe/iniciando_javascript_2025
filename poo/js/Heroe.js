fb1802b1c92bd73826611e3b23f4934c79d41470
gallopeladoprofe <juanftp100.edu@gmail.com>
2025-02-19 19:25:45 -0300

    fix: se claseo heroe y se limpio poo

---
+++ poo/js/Heroe.js
@@ -0,0 +1,8 @@
+class Heroe {
+    constructor(tipoDeAtaque, atributoPrincipal, habilidades, ataque) {
+        this.atributoPrincipal = atributoPrincipal;
+        this.tipoDeAtaque = tipoDeAtaque;
+        this.habilidades = habilidades;
+        this.ataque = ataque;
+    }
+}
--- poo/js/poo.js
+++ poo/js/poo.js
@@ -65,13 +65,4 @@ class Persona {
 
 }
 
-class Heroe {
-    constructor(tipoDeAtaque, atributoPrincipal, habilidades, ataque) {
-        this.atributoPrincipal = atributoPrincipal;
-        this.tipoDeAtaque = tipoDeAtaque;
-        this.habilidades = habilidades;
-        this.ataque = ataque;
-    }
-}
-
 class CrystalMaiden {}
